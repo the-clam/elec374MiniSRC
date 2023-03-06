558315c8cdbe344e313fced8b172e349962f6efa
Carl L <carl.lam@queensu.ca>
2023-03-06 15:35:19 -0500

    fix

--- reg32_mdr.v
+++ reg32_mdr.v
@@ -2,16 +2,13 @@
 module reg32_mdr(
 	input wire [31:0] MDMuxIn0, MDMuxIn1,
 	input wire MDMux_sel, clr, clk, en,
-	output reg [31:0] Q
+	output wire [31:0] Q
 );
 	wire [31:0] MDR_mux_output; // data from MDR mux to be inputted to the reg
 	mux_2_to_1 MDR_mux(
 		.mux_input_0(MDMuxIn0), .mux_input_1(MDMuxIn1), .mux_sel(MDMux_sel), .mux_out(MDR_mux_output)
 	);
-	initial Q = 0;
-	always@(posedge clk)
-	begin
-		if(en) Q[31:0] <= MDR_mux_output;
-		if(clr) Q[31:0] <= 32'b0;
-	end
+	reg32 MDR_reg(
+		.clr(clr), .clk(clk), .en(en), .D(MDR_mux_output), .Q(Q)
+	);
 endmodule
